37ce8c06b3a07a9f4db90f239764987197706498
Tyler Nguyen <0tylernguyen@gmail.com>
2023-05-16 12:40:10 -0700

    slash command test

--- GetItDone.py
+++ GetItDone.py
@@ -2,27 +2,40 @@
 
 import discord
 import os
+import sqlite3
+# from discord import app_commmands
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+# con = sqlite3.conntect("database.db")
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} commands")
+    except Exception as e:
+        print(e)
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if message.content.startswith('.'):
         await message.channel.send('Hello!')
 
-client.run(TOKEN)
+@bot.tree.command(name="hello")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message("HI")
+
+bot.run(TOKEN)
